da6c2ee5094ea277d7d8c93d24b320347ae99d04
LindaTich <159271838+LindaTich@users.noreply.github.com>
2024-03-03 16:44:34 +0000

    Add files via upload

---
+++ conversions.py
@@ -0,0 +1,29 @@
+'''Start:
+Create a file called conversions in python
+Save it in a folder.
+Create pseudocode confirming the steps to be taken.
+Convert the variable values using casting.
+Cast variable num1 from a float into a whole number using int.
+Print num1.
+Cast variable num2 from an int into a decimal number using float.
+Print num2.
+Cast variable num3 from an int into a string using str.
+Print num3.
+Cast variable string1 from a str into an int.
+Print string1.
+End '''
+
+num1 = int(99.23)
+print(num1)
+
+num2 = float(23)
+print(num2)
+
+num3 = str(150)
+print(num3)
+
+string1 = int("100")
+print(string1)
+
+# The above shows variable values and casting as is described in the pseudocode at the start.
+
